7453ec9021679163dca047259ecf4699407744fe
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2025-04-06 21:40:30 -0300

    feat: :sparkles: función para determinar si un periodo es máximo, docstrings e imports

--- tp3-aleatoriedad-montecarlo/generators.py
+++ tp3-aleatoriedad-montecarlo/generators.py
@@ -8,6 +8,9 @@ Contiene a los generadores de:
     - Congruencial Lineal Multiplicativo
 """
 
+import math
+from utils import prime_factor_descomposition
+
 
 class Von_Neumann:
     def generator(self, n: int) -> int:
@@ -47,7 +50,40 @@ class Von_Neumann:
 
 class LinearCongruentialMixed:
     def linear_congruential_mixed(self, a: int, c: int, M: int, y_i: int) -> int:
+        """
+        Generador Congruencial Lineal Mixto
+
+        Args:
+            a (int): parámetro del generador congruencial
+            c (int): parámetro del generador congruencial
+            M (int): cota superior de la secuencia
+            y_i (int): valor de la i-ésima iteración
+
+        Returns:
+            int: Devuelve la iteración i+1-ésima de la secuencia.
+        """
         return (a*y_i+c) % M
+
+    def is_maximum_period(self, a: int, c: int, M: int, ) -> bool:
+        """
+        Determina si se puede generar una secuencia con periodo máximo
+
+        Args:
+            a (int): parámetro del generador congruencial
+            c (int): parámetro del generador congruencial
+            M (int): cota superior de la secuencia
+
+        Returns:
+            bool: True en caso de poder generar un periodo máximo, False en caso contrario.
+        """
+        prime_descomposition = prime_factor_descomposition(n=M)
+        factor_min_prime = min(prime_descomposition)
+        is_maximum = False
+        if math.gcd(c, M) == 1:
+            if a % factor_min_prime == 1:
+                if M % 4 == 0 and a % 4 == 1:
+                    is_maximum = True
+        return is_maximum
 
 
 class LinearCongruentialMultiplicative:
